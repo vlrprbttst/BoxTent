a66677f2632ef89bc6fdf8950aa14bbc71558e37
valerio.pierbattista <valerio.pierbattista@doing.com>
2020-11-20 11:08:38 +0100

    popolo grunt build

--- gruntfile.js
+++ gruntfile.js
@@ -171,15 +171,6 @@ module.exports = function(grunt) {
             }
         },
         copy: {
-            the_html: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= source %>',
-                    dest: '<%= dev %>/',
-                    src: ['**/*.html']
-                }]
-            },
             the_fonts: {
                 files: [{
                     expand: true,
@@ -242,6 +233,6 @@ module.exports = function(grunt) {
     // default for development: type grunt
     grunt.registerTask('default', ['browserSync', 'watch']);
     // rebuild the _dev folder: type grunt build
-    grunt.registerTask('build', ['clean']);
+    grunt.registerTask('build', ['clean','processhtml:dev','imagemin','copy:unoptimizedImage','copy:js','sass:dist','postcss:dev','copy:the_fonts','copy:favicons']);
 
 };
